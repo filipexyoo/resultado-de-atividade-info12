4516b0d4e751afa57dbdd7f898a0e8c6074a6dad
Filipe <filipe13m.alves@gmail.com>
2026-05-12 15:00:11 -0300

    Add files via upload

---
+++ desafio 3/script.js
@@ -0,0 +1,24 @@
+// DESAFIO 3 — APROVAÇÃO ESCOLAR
+
+// Crie variáveis para:
+// nota
+// frequencia
+
+// O sistema deve verificar se:
+// A nota é maior ou igual a 7
+// E a frequência é maior ou igual a 75
+
+// Se as duas condições forem verdadeiras:
+// Mostrar "Aluno aprovado"
+
+// Caso contrário:
+// Mostrar "Aluno reprovado"
+
+let nota = 1
+let frequencia = 90;
+
+if (nota >= 10 && frequencia >=90) {
+    console.log("Aluno aprovado")
+} else {
+    console.log("Aluno reprovado")
+}
