5845c1c351333cc92bd6bb41f8f789bbc46bfc1d
Filipe <filipe13m.alves@gmail.com>
2026-05-13 20:17:44 -0300

    Add files via upload

---
+++ desafio 9/script.js
@@ -0,0 +1,20 @@
+// DESAFIO 2 — PRODUTO DISPONÍVEL
+
+// Crie uma variável:
+// produtoEsgotado
+
+// O sistema deve verificar se:
+// O produto NÃO está esgotado
+
+// Se não estiver esgotado:
+// Mostrar "Produto disponível para compra"
+
+// Caso contrário:
+// Mostrar "Produto indisponível"
+
+let produtoEsgotado = true
+if (!produtoEsgotado) {
+    console.log("Produto disponível para compra")
+} else {
+    console.log("produto indisponível")
+}
