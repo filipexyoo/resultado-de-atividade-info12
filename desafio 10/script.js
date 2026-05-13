937dc9eb20680a5d2100f59084a703aef75dfcb9
Filipe <filipe13m.alves@gmail.com>
2026-05-13 20:28:33 -0300

    Add files via upload

---
+++ desafio 10/script.js
@@ -0,0 +1,20 @@
+// DESAFIO 3 — SISTEMA FUNCIONANDO
+
+// Crie uma variável:
+// sistemaOffline
+
+// O sistema deve verificar se:
+// O sistema NÃO está offline
+
+// Se estiver funcionando:
+// Mostrar "Sistema online"
+
+// Caso contrário:
+// Mostrar "Sistema indisponível"
+
+let sistemaOffline = false;
+if (!sistemaOffline) {
+    console.log("Sistema online");
+} else {
+    console.log ("Sistema indisponível");
+}
