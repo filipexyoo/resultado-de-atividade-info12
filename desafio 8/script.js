89ed590f52b2fd307178294c44b55d9ed9445203
Filipe <filipe13m.alves@gmail.com>
2026-05-13 20:09:45 -0300

    Add files via upload

---
+++ desafio 8/script.js
@@ -0,0 +1,21 @@
+// DESAFIO 1 — USUÁRIO BLOQUEADO
+
+// Crie uma variável:
+// usuarioBloqueado
+
+// O sistema deve verificar se:
+// O usuário NÃO está bloqueado
+
+// Se não estiver bloqueado:
+// Mostrar "Usuário pode acessar o sistema"
+
+// Caso contrário:
+// Mostrar "Usuário bloqueado"
+
+let usuarioBloqueado = false
+if (!usuarioBloqueado) {
+    console.log("Usuário pode acessar o sistema")
+}
+else {
+    console.log("Usuário bloqueado")
+}
